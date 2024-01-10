f04e1a2a02e3027e494d9bdd0debe72e85ec962c
Nishant5789 <nishantbhandari218@gmail.com>
2024-01-10 23:24:00 +0530

    added backed api  with editor

--- fronted/src/features/codeeditor/components/Monaco.js
+++ fronted/src/features/codeeditor/components/Monaco.js
@@ -5,11 +5,13 @@ import Navbar from './Navbar';
 import Axios from 'axios';
 import spinner from '../../../app/spinner.svg';
 import { useDispatch, useSelector } from 'react-redux';
+import { ExcuteCodeAsync, selectJobId } from '../codeSlice';
 import axios from 'axios';
 
 function App({ProblemId}) {
 	const dispatch = useDispatch();
 
+	const JobId = useSelector(selectJobId);
 	const [userCode, setUserCode] = useState(``);
 	const [userLang, setUserLang] = useState("cpp");
 	const [userTheme, setUserTheme] = useState("vs-dark");
@@ -24,14 +26,53 @@ function App({ProblemId}) {
 		fontSize: fontSize
 	}
 
-	
+	function compile() {
+		setLoading(true);
+		if (userCode === ``) {
+			return
+		}
+		dispatch(ExcuteCodeAsync({
+			ProblemId,
+			Language:userLang,
+			codeData:userCode
+		}))
+		setIsPolling(true);
+	}
 
 	// Function to clear the output screen
 	function clearOutput() {
 		setUserOutput("");
 	}
 
-	
+	useEffect(()=>{
+		if(JobId!=="" && isPolling){
+		const pollInterval = setInterval(async () => {
+			const { data: statusRes } = await axios.get(
+				`http://localhost:8080/codeRunner/status?jobId=${JobId}`,
+				);
+				const { success, job, error } = statusRes;
+				console.log(success);
+			if (success) {
+			  const { Status: jobStatus, Output: jobOutput } = job;
+			  console.log(jobStatus);
+			  if (jobStatus === "pending") return;
+			  console.log("done");
+			  setUserOutput(jobOutput);
+			  setStatus("executed");
+			  setIsPolling(false);
+			  setLoading(false);
+			  clearInterval(pollInterval);
+			} else {
+			  console.error(error);
+			  setUserOutput(error);
+			  setStatus("Bad request");
+			  setIsPolling(false);
+			  setLoading(false);
+			  clearInterval(pollInterval);
+			}
+		}, 1000);
+	}
+},[JobId])
 
 	return (
 		<div className="App pb4-">
